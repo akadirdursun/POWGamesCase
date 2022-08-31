0ccac589ce7ea44bf6478cb9fa0af3d2ce6273ca
akadirdursun <akadirdursn@gmail.com>
2022-08-31 20:14:24 +0300

    Rotation System

---
+++ PowGamesCaseStudy/Assets/Scripts/CubeStackRotater.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CubeMatch
+{
+    public class CubeStackRotater : MonoBehaviour
+    {
+        [SerializeField] private float rotateSpeed;
+
+        #region MonoBehaviour METHODS
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Rotate();
+            }
+        }
+        #endregion
+
+        private void Rotate()
+        {
+            float xRotation = -Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+            float yRotation = -Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+
+            transform.RotateAround(transform.position, Vector3.up, xRotation);
+            transform.RotateAround(transform.position, Vector3.left, yRotation);
+        }
+    }
+}
